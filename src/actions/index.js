1a352bf73c2964e92529ec7bfa62b5ee6be96833
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-10-04 11:14:47 +0300

    Redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -56,3 +56,10 @@ export const heroDeleted = (id) => {
         payload: id
     }
 }
+
+export const fetchHeroes = (request) => (dispatch) => {
+    dispatch(heroesFetching());
+    request("http://localhost:3001/heroes")
+        .then(data => dispatch(heroesFetched(data)))
+        .catch(() => dispatch(heroesFetchingError()))
+}
--- src/store/index.js
+++ src/store/index.js
@@ -12,21 +12,6 @@ const stringMiddleware = () => (next) => (action) => {
     return next(action)
 }
 
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if (typeof action === "string") {
-            return oldDispatch({
-                type: action
-            })
-        }
-        return oldDispatch(action)
-    }
-    return store
-}
-
 const store = createStore(
                     combineReducers({heroes, filters}),
                     compose(applyMiddleware(ReduxThunk, stringMiddleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
