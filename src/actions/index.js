98a5f6af5d01fc3fe403b10a05e202d8150a357b
Kirill Tsonev <kirill.tsonev.work@gmail.com>
2022-10-05 13:26:51 +0300

    createSlice

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,4 @@
-import { createAction } from "@reduxjs/toolkit"
-
-export const heroesFetching = createAction('HEROES_FETCHING')
-
-export const heroesFetched = createAction('HEROES_FETCHED')
-
-export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR')
-
-export const filtersFetching = createAction('FILTERS_FETCHING')
-
-export const filtersFetched = createAction('FILTERS_FETCHED')
-
-export const filtersFetchingError = createAction('FILTERS_FETCHING_ERROR')
-
-export const activeFilterChanged = createAction('ACTIVE_FILTER_CHANGED')
-
-export const heroCreated = createAction('HERO_CREATED')
-
-export const heroDeleted = createAction('HERO_DELETED')
+import {heroesFetching, heroesFetched, heroesFetchingError} from "../components/heroesList/heroesSlice"
 
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching());
--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import { filtersFetching, filtersFetched, filtersFetchingError, activeFilterChanged } from '../../actions';
+import { filtersFetching, filtersFetched, filtersFetchingError, activeFilterChanged } from '../heroesFilters/filtersSlice';
 import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
